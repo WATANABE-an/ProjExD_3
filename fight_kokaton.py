3fe1fd0133d2066296cfb3e701597f266d8eca1d
c0a25278ee <c0a25278ee@edu.teu.ac.jp>
2026-04-28 14:43:32 +0900

    練習４ゲームオーバー

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -95,8 +95,8 @@ class Beam:
         """
         self.img = pg.image.load(f"fig/beam.png")
         self.rct = self.img.get_rect()
-        self.rct.centery = bird.rct.centery
-        self.rct.left = bird.rct.right
+        self.rct.centery = bird.rct.centery  # ビームの中心縦座標 = こうかとんの中心縦座標
+        self.rct.left = bird.rct.right  # ビームの左座標 = こうかとんの右座標
         self.vx, self.vy = +5, 0
 
     def update(self, screen: pg.Surface):
@@ -161,6 +161,9 @@ def main():
         if bomb is not None:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
                 bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
@@ -168,17 +171,18 @@ def main():
         
         if bomb is not None:
             if beam is not None:
-                if beam.rct.collidedict(bomb.rct):
-                    bomb = None
+                if beam.rct.colliderect(bomb.rct):  
                     beam = None
-                    bird.change_img(6, screen)
+                    bomb = None
+                    bird.change_img(6, screen)  
                     pg.display.update()
                     time.sleep(1)
+
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
-            beam.update(screen)   
+            beam.update(screen)
         if bomb is not None:
             bomb.update(screen)
         pg.display.update()
